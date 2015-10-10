dd73376f6770ba94accf5b99cafccc9241f621a0
Duckzbug <domduxbury@hotmail.com>
2015-10-10 22:46:49 +0100

    Deleted useless code

--- MeteorHackathon2015.js
+++ MeteorHackathon2015.js
@@ -1,13 +1,5 @@
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
   Template.test.events({
     'click .test-button': function () {
       // increment the counter when button is clicked
