9391179c9701dfe3fcbeeb2c2d154c3416a1507a
Duckzbug <domduxbury@hotmail.com>
2015-10-11 15:41:35 +0100

    Jquery for box in place

--- client/surprise/surprise.js
+++ client/surprise/surprise.js
@@ -6,17 +6,28 @@ Template.surprise.events({
     positiveFormFeedback = $('.positive-form-feedback');
     inputValue = parseInt(inputValue);
       if (isNaN(inputValue)) {
+      	negativeFormFeedback.show();
         negativeFormFeedback.text("Input must be an integer");
-        positiveFormFeedback.text("");        
+        positiveFormFeedback.text("");
+        positiveFormFeedback.hide();        
         return;
       } else if(inputValue < 1) {
+      	negativeFormFeedback.show();
       	negativeFormFeedback.text("Input must be greater than £1");
-      } 
+      } else if (inputValue > 1000) {
+      	negativeFormFeedback.show();
+      	negativeFormFeedback.text("Input must be less than £1000");
+      }
       else {
       	negativeFormFeedback.text("");
+      	negativeFormFeedback.hide();
+      	positiveFormFeedback.show();
+      	positiveFormFeedback.text("Loading...");
       	NProgress.start();
       	Meteor.call('createShoppingCart', inputValue*100, function(error, result) {
-      	  positiveFormFeedback.text(result);
+      	  claimPrizeImage = $('.claim-prize-image');
+      	  claimPrizeImage.attr("href", result);
+      	  positiveFormFeedback.text("Sucess! Your request has been processed! Click on the box below to claim your surprise");
       	  NProgress.done();
       	});
       	return;  	
