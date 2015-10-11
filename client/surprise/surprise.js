13c61e7c81d9cb9f03c17d283553d41a4742b57e
Duckzbug <domduxbury@hotmail.com>
2015-10-11 15:59:40 +0100

    Show prize on succcess

--- client/surprise/surprise.js
+++ client/surprise/surprise.js
@@ -25,6 +25,7 @@ Template.surprise.events({
       	positiveFormFeedback.text("Loading...");
       	NProgress.start();
       	Meteor.call('createShoppingCart', inputValue*100, function(error, result) {
+      	  claimPrizeImage.show();
       	  claimPrizeImage = $('.claim-prize-image');
       	  claimPrizeImage.attr("href", result);
       	  positiveFormFeedback.text("Sucess! Your request has been processed! Click on the box below to claim your surprise");
